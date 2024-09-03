60392392d4814cd4f226f2a6229971cf5cc50aad
Victor <victorpassos2012@gmail.com>
2024-09-03 18:42:11 -0300

    Desafio 5 feito

--- Desafios/Desafio-5.js
+++ Desafios/Desafio-5.js
@@ -6,3 +6,27 @@ a) Essa string pode ser informada através de qualquer entrada de sua preferênc
 b) Evite usar funções prontas, como, por exemplo, reverse;*/
 
 
+const stringOriginal = 'String normal';
+
+function Stringinverter(str) {
+
+    let arr = str.split('');
+
+    let resultado;
+ 
+    for (let i = arr.length - 1; i >= 0; i--) {
+        resultado += arr[i];
+    }
+    return resultado;
+}
+
+const stringInvertida = Stringinverter(stringOriginal);
+
+console.log('String Original:', stringOriginal);
+console.log('String Invertida:', stringInvertida);
+
+/*
+Utilizando String normal como a string de exemplo, o codigo retorna:
+String Original: String normal
+String Invertida: undefinedlamron gnirtS
+*/
